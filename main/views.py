abaa7f76136afb35dbf235673612b106c3f9ce75
lucindalaurent <lucindalaurent21@gmail.com>
2023-09-13 06:49:23 +0700

    follow the instruction

--- main/views.py
+++ main/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def render_main(request):
     context = {
-        'name': 'Cheesecake',
-        'desc': 'A dessert consisting of a thick, creamy filling of cheese, eggs, and sugar over a thinner crust and topped with sweet or sometimes salty items.'
+        'appname': 'CakeStock',
+        'name': 'Lucinda Laurent',
+        'class': 'PBP A'
     }
 
     return render(request, "main.html", context)
